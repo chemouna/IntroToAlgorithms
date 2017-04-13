b2a1eaf88c9c65e14fd77eb4e05b2787dce13205
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-04-14 00:25:59 +0200

    Add some example for two stacks example.

--- src/com/mounacheikhna/introalgo/chapter10/TwoStack.java
+++ src/com/mounacheikhna/introalgo/chapter10/TwoStack.java
@@ -3,13 +3,13 @@ package com.mounacheikhna.introalgo.chapter10;
 import java.util.Arrays;
 import java.util.EmptyStackException;
 
-public class DoubleStack<T> {
+public class TwoStack<T> {
     T[] stack;
     int idx1;
     int idx2;
 
     @SuppressWarnings("unchecked")
-    public DoubleStack(int size) {
+    public TwoStack(int size) {
         stack = (T[]) new Object[size];
         idx1 = -1;
         idx2 = size - 1;
@@ -17,7 +17,7 @@ public class DoubleStack<T> {
 
     boolean push1(T object) {
         int nextIdx = idx1 + 1;
-        if (nextIdx >= idx2 || nextIdx >= stack.length) return false;
+        if (idx2 <= nextIdx || nextIdx >= stack.length) return false;
         stack[++idx1] = object;
         return true;
     }
@@ -49,25 +49,40 @@ public class DoubleStack<T> {
 
     public static void main(String[] args) {
         System.out.println("init:");
-        DoubleStack<Integer> doubleStack = new DoubleStack<>(5);
-        System.out.println("  " + doubleStack);
+        TwoStack<Integer> twoStack = new TwoStack<>(5);
+        System.out.println("  " + twoStack);
+        System.out.println();
+        System.out.println("push 3 values to stack 1:");
+        for (int i = 0; i < 3; ++i) {
+            twoStack.push1(i);
+        }
+        System.out.println("  " + twoStack);
+        System.out.println();
+        System.out.println("push 5 values to stack 2:");
+        for (int i = 0; i < 5; ++i) {
+            twoStack.push2(i);
+        }
+        System.out.println("  " + twoStack);
+        System.out.println("(some elements couldn't fit)");
+        System.out.println();
+
         System.out.println("pop 7 elements from stack 1:");
         for (int i = 0; i < 7; ++i) {
             try {
-                int popped = doubleStack.pop1();
+                int popped = twoStack.pop1();
                 System.out.println("popped: " + popped);
             } catch (EmptyStackException e) {
                 System.out.println("couldn't pop anymore");
                 break;
             }
         }
-        System.out.println("  " + doubleStack);
+        System.out.println("  " + twoStack);
         System.out.println();
 
         System.out.println("pop 1 element from stack 2:");
-        int popped = doubleStack.pop2();
+        int popped = twoStack.pop2();
         System.out.println("popped: " + popped);
-        System.out.println("  " + doubleStack);
+        System.out.println("  " + twoStack);
         System.out.println();
     }
 }
