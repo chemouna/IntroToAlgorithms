3ee6bf5a37a098eb7bc864cf70f1b68256dd7413
Mouna Cheikhna <cheikhnamouna@gmail.com>
2017-04-16 13:54:56 -0700

    Start implementation of Stack with two queues.

--- src/com/mounacheikhna/introalgo/chapter10/StackWithTwoQueues.java
+++ src/com/mounacheikhna/introalgo/chapter10/StackWithTwoQueues.java
@@ -4,5 +4,28 @@ package com.mounacheikhna.introalgo.chapter10;
  * Created by m.cheikhna on 16/04/2017.
  */
 public class StackWithTwoQueues<T> {
+    Queue<T> q1;
+    Queue<T> q2;
+
+    public StackWithTwoQueues() {
+        q1 = new Queue<>();
+        q2 = new Queue<>();
+    }
+
+    public void push(T item) {
+        q1.enqueue(item);
+    }
+
+    public T pop() {
+        return q2.dequeue();
+    }
+
+    public boolean isEmpty() {
+        return q1.isEmpty() && q2.isEmpty();
+    }
+
+    public int size() {
+        return q1.size() + q2.size();
+    }
 
 }
